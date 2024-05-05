63ee8f0ed8873b49450d464e06d063a8a8034e6e
Sedat <sedatkotan06@gmail.com>
2024-05-05 23:34:53 +0300

    View Components added for home page

---
+++ Presentation/OnlineShopping.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopping.WebUI.ViewComponents.UILayoutViewComponents;
+
+public class _NavbarUILayoutComponentPartial:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
