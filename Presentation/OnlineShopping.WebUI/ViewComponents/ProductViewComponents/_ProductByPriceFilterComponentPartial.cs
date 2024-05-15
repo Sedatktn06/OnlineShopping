a65fd9f12a4775e09ebf5be2dd95e63d3c54291b
Sedat <sedatkotan06@gmail.com>
2024-05-15 23:24:41 +0300

    Page, controller and partial views added for Product entity

---
+++ Presentation/OnlineShopping.WebUI/ViewComponents/ProductViewComponents/_ProductByPriceFilterComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopping.WebUI.ViewComponents.ProductViewComponents;
+
+public class _ProductByPriceFilterComponentPartial:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
