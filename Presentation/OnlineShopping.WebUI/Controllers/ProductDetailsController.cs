c49cadee6662980b1defb6825ba1bcba1af1d242
Sedat <sedatkotan06@gmail.com>
2024-05-18 23:59:20 +0300

    commit

---
+++ Presentation/OnlineShopping.WebUI/Controllers/ProductDetailsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopping.WebUI.Controllers;
+
+public class ProductDetailsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
