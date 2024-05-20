fc56c0d15db6349844d01cf7deb11739fcf1c27a
Sedat <sedatkotan06@gmail.com>
2024-05-20 23:03:02 +0300

    View Components added for Layout

---
+++ Presentation/OnlineShopping.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeadComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopping.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents;
+
+public class _AdminLayoutHeadComponentPartial:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
